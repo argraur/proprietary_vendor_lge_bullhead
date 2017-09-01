b4d03a0f4148c48647fcb3ecfaa2098593fabd13
argraur <argraur@gmail.com>
2017-09-01 16:20:09 +0000

    bullhead-vendor: Build vendorimage
    WARNING! I'm very lazy so just copying whole dirs :) (will do normal thing later)

---
+++ pixelrom-only-vendorimage.mk
@@ -0,0 +1,9 @@
+# WARNING! THIS MAKEFILE NEEDS PATCH FOR DEFINITIONS TO COPY WHOLE DIRS
+PRODUCT_COPY_FILES += \
+	vendor/lge/bullhead/vendorimage/bin:vendor/bin \
+	vendor/lge/bullhead/vendorimage/etc:vendor/etc \
+	vendor/lge/bullhead/vendorimage/firmware:vendor/firmware \
+	vendor/lge/bullhead/vendorimage/lib:vendor/lib \
+	vendor/lge/bullhead/vendorimage/lib64:vendor/lib64 \
+	vendor/lge/bullhead/vendorimage/media:vendor/media \
+	vendor/lge/bullhead/vendorimage/manifest.xml:vendor/manifest.xml
